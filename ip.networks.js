2581ac4ddcd3fa7df7dcbc78ac8091d2fb452f1f
Gil <gilles.antonetti@pm.gouv.fr>
2019-04-16 19:59:59 +0000

    msg

---
+++ ip.networks.js
@@ -0,0 +1,9 @@
+const ttl = { ttl:25 }
+
+module.exports = {
+  nets24: {
+    '190.10.10.0': { tag:'TG1', rtr:'RTR', rtt:40 },
+    '190.10.11.0': { tag:'TG1', rtr:'RTR', rtt:40 },
+    '190.10.12.0': { tag:'TG2', rtr:'RTR', rtt:40 },
+  }
+}
