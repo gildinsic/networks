29ff93386f9eb789dc5c327329f2b1b933a219af
Gil <gilles.antonetti@pm.gouv.fr>
2019-04-16 20:02:25 +0000

    msg

--- ip.networks.js
+++ ip.networks.js
@@ -2,6 +2,9 @@ const ttl = { ttl:25 }
 
 module.exports = {
   nets24: {
+
+   // !!!!!!!      this is a random prefix
+
     '190.10.10.0': { tag:'TG1', rtr:'RTR', rtt:40 },
     '190.10.11.0': { tag:'TG1', rtr:'RTR', rtt:40 },
     '190.10.12.0': { tag:'TG2', rtr:'RTR', rtt:40 },
